a6fb45fd0577307f6fa201421ad8275f9a508895
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2018-11-02 05:07:55 +0000

    finish sort

--- algorithms/test/bubbleSort.spec.js
+++ algorithms/test/bubbleSort.spec.js
@@ -6,7 +6,22 @@ let expect = chai.expect;
 
 describe('#bubbleSort', function(){
     it('should sort an array from least to greatest', function(){
+        let unsorted = [2,1];
+        assert.equal(JSON.stringify(bubbleSort(unsorted)), JSON.stringify([1,2]));
+    })
+    it('should sort an array from least to greatest', function(){
+        let unsorted = [3,2,1];
+        assert.equal(JSON.stringify(bubbleSort(unsorted)), JSON.stringify([1,2,3]));
+    })
+    it('should sort an array from least to greatest', function(){
         let unsorted = [5,3,2,1];
         assert.equal(JSON.stringify(bubbleSort(unsorted)), JSON.stringify([1,2,3,5]));
+    })
+})
+
+describe('#switchArrElements', function(){
+    it('should switch 2 array elements', function(){
+        let unswitched = [2,1];
+        assert.equal(JSON.stringify(switchArrElements(0,1,unswitched)), JSON.stringify([1,2]));
     })
 })
