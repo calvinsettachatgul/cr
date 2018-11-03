626c6f34fcf57e7e76a777e0babcb2356a5c3520
Calvin Settachatgul <calvin.settachatgul@autodesk.com>
2018-11-02 19:47:28 -0700

    add a couple more tests

--- algorithms/test/bubbleSort.spec.js
+++ algorithms/test/bubbleSort.spec.js
@@ -5,6 +5,14 @@ let chai = require('chai');
 let expect = chai.expect;
 
 describe('#bubbleSort', function(){
+    it('should be able to handle an empty array and return []', function(){
+        let unsorted = [];
+        assert.equal(JSON.stringify(bubbleSort([])), JSON.stringify([]));
+    })
+    it('should be able to handle an array of size 1', function(){
+        let unsorted = [1];
+        assert.equal(JSON.stringify(bubbleSort([1])), JSON.stringify([1]));
+    })
     it('should sort an array from least to greatest', function(){
         let unsorted = [2,1];
         assert.equal(JSON.stringify(bubbleSort(unsorted)), JSON.stringify([1,2]));
