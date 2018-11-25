98029ed002cc245bf6039aebc3f541246f904f3f
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2018-11-25 15:29:14 -0800

    feature-6-4-algo add 3 problems

---
+++ algorithms/left_rotate.js
@@ -0,0 +1,45 @@
+// https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+//
+/*
+
+Check out the resources on the page's right side to learn more about arrays. The video tutorial is by Gayle Laakmann McDowell, author of the best-selling interview book Cracking the Coding Interview.
+
+  https://www.amazon.com/gp/product/0984782850/ref=as_li_tl?ie=UTF8&tag=hrctci-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=0984782850&linkId=4b0bb75d87b94d4378dd3bd245a7284b
+  
+  A left rotation operation on an array shifts each of the array's elements 1 unit to the left. For example, if 2 left rotations are performed on array [1,2,3,4,5] , then the array would become [3,4, 5, 1,2 ].
+
+  Given an array a of n integers and a number, d , perform d left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+
+  Function Description
+
+Complete the function rotLeft in the editor below. It should return the resulting array of integers.
+
+  rotLeft has the following parameter(s):
+
+An array of integers a.
+  An integer d, the number of rotations.
+  Input Format
+
+The first line contains two space-separated integers n and d, the size of a and the number of left rotations you must perform. 
+  The second line contains n space-separated integers a[i].
+
+  Constraints
+
+Output Format
+
+Print a single line of n space-separated integers denoting the final state of the array after performing d left rotations.
+
+  Sample Input
+
+5 4
+1 2 3 4 5
+Sample Output
+
+5 1 2 3 4
+Explanation
+
+When we perform  left rotations, the array undergoes the following sequence of changes:
+
+[1,2,3,4,5] => [2,3,4,5,1] => [3,4,5,1,2] => [4,5,1,2,3] => [5,1,2,3,4]
+
+*/
---
+++ algorithms/min_max_product.js
@@ -0,0 +1,24 @@
+/*
+ * https://www.codewars.com/kata/minimum-and-maximum-product-of-k-elements
+ *
+ * Given a list of numbers (possibly including duplicates) and a positive integer k, find the minimum and maximum possible product of k elements taken from the list.
+ *
+ * If you cannot take enough elements from the list (k > list size), return None/nil
+ *
+ * Examples
+ * numbers = [1, -2, -3, 4, 6, 7]
+ *
+ * k = 1  ==>  -3, 7
+ * k = 2  ==>  -21, 42    # -3*7, 6*7
+ * k = 3  ==>  -126, 168  # -3*6*7, 4*6*7
+ * k = 7  ==>  None       # there are only 6 elements in the list
+ * Note: the test lists are sufficiently small (up to 20 elements) for a simple approach.
+ *
+ * There is an advanced version of this kata, if you're up to the challenge! :-)
+ *
+ * My other katas
+ * If you enjoyed this kata then please try my other katas! :-)
+ *
+ * Translations are welcome!
+ *
+ * */
