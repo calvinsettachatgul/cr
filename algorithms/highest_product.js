6baec62f50dcebd701d8636d5bf4bb1cf0a31795
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2018-12-17 15:39:31 -0800

    feature-8: add solution files

--- algorithms/highest_product.js
+++ algorithms/highest_product.js
@@ -4,10 +4,35 @@
 //
 //
 
+const findMaxExcluding = (arr, indeces) => {
+  let max = 1; 
+  let index = null;
+  for( let i = 0 ; i< arr.length; i++){
+    if(arr[i] > max && !indeces[i]){
+      max = arr[i] 
+      index = i;
+    };
+  };
+  return {
+    index,
+    max,
+  }
+};
+
 const highestProduct = (arr) => {
-  let factors = {}
-  
-  return Math.max(...arr);
+  let maxFactors = [1,1,1];
+  let indeces = {};
+  let collected = {}
+  for (let i = 0 ; i< 3; i++){
+    let maxData = findMaxExcluding(arr, indeces); // find the maximum number excluding an index
+    indeces[maxData.index] = maxData;
+  }
+  let maxProduct = 1;
+  Object.keys(indeces).forEach((index) => {
+    maxProduct *= indeces[index].max;
+  });
+  return maxProduct
 };
 
 module.exports = highestProduct;
+
--- algorithms/test/highest_product.spec.js
+++ algorithms/test/highest_product.spec.js
@@ -3,11 +3,23 @@ let assert = require('assert');
 let chai = require('chai');
 let expect = chai.expect;
 
-describe('#highestProduct', function () {
+describe.only('#highestProduct', function () {
   it('should return highest product of 3 numbers in array', function () {
     expect(highestProduct([1,2,3])).to.equal(6);
   });
   it('should return highest product of 3 numbers in array', function () {
     expect(highestProduct([1,2,3,4])).to.equal(24);
+  });
+  it('should return highest product of 3 numbers in array', function () {
+    expect(highestProduct([1,2,4,4,4])).to.equal(4 * 4 * 4);
+  });
+  it('should return highest product of 3 numbers in array', function () {
+    expect(highestProduct([1,2,4,9,100])).to.equal(4 * 9 * 100);
+  });
+  it('should return highest product of 3 numbers in array', function () {
+    expect(highestProduct([1000,2,4,9,100])).to.equal(1000 * 9 * 100);
+  });
+  it('should return highest product of 3 numbers in array', function () {
+    expect(highestProduct([1,1000,4,9,100])).to.equal(1000 * 9 * 100);
   });
 });
