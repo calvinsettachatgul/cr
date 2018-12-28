6002384e5689f39ac4048f92557d8fb3795fa52a
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2018-12-28 02:39:49 +0000

    change highest product

--- algorithms/highest_product.js
+++ algorithms/highest_product.js
@@ -11,6 +11,9 @@ const findMaxExcluding = (arr, indeces) => {
     if(arr[i] > max && !indeces[i]){
       max = arr[i] 
       index = i;
+    //   { max: 2, index:1}
+    //   { max: 3, index:2}
+    //   { max: 4, index:3}
     };
   };
   return {
@@ -19,10 +22,19 @@ const findMaxExcluding = (arr, indeces) => {
   }
 };
 
+// arr       indeces
+// 0 [5,2,3,4] {}
+// 1 find 5 {index:0,max:5} {0: {index:0, max:5}}
+// 2 find 4 {index:3,max:5} {3: {index:3, max:4},0: {index:0, max:5} }
+
+// indeces = {
+//   0: {index:0, max 5},
+//   3: {index:3, max 4},
+//   2: {index:2, max 2},
+// }
+
 const highestProduct = (arr) => {
-  let maxFactors = [1,1,1];
   let indeces = {};
-  let collected = {}
   for (let i = 0 ; i< 3; i++){
     let maxData = findMaxExcluding(arr, indeces); // find the maximum number excluding an index
     indeces[maxData.index] = maxData;
