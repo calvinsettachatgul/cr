c8b268c4e7f38617b8c4ce3779244cccbc76d5e9
robinchoii <robinchoii@gmail.com>
2019-01-10 19:30:22 -0800

    add solution to reverse inplace and tests

--- algorithms/test/reverse_inplace.spec.js
+++ algorithms/test/reverse_inplace.spec.js
@@ -1,1 +1,22 @@
-reverse_inplace.spec.js
+let reverseInplace = require('../reverse_inplace');
+let assert = require('assert');
+let chai = require('chai');
+let expect = chai.expect;
+
+console.log(reverseInplace(['a','b','c','d']))
+
+describe('#reverseInplace', function () {
+  it('should return reversed order of characters in an array', function () {
+    let arr = ['a','b','c','d']
+    let reversed_arr = ['d','c','b','a']
+    assert.notStrictEqual(reverseInplace(arr), reversed_arr);
+  });
+  context('array length is 1', function() {
+      it('should return original array', function () {
+        let arr = ['a']
+        let reversed_arr = ['a']
+        assert.notStrictEqual(reverseInplace(arr), reversed_arr);
+      });
+  });
+  // try JSON.stringify
+});
